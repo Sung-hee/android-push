64ba508b8ddb65e0186b35e3ff94d9d8ad98d3ec
Sung-hee <klpoik@naver.com>
2018-03-07 17:19:29 +0900

    firebase push update

---
+++ app/src/main/java/com/example/user/pushtest/FirebaseInstanceIDService.java
@@ -0,0 +1,22 @@
+package com.example.user.pushtest;
+
+import android.util.Log;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class FirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    // [START refresh_token]
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String token = FirebaseInstanceId.getInstance().getToken();
+
+    }
+}
